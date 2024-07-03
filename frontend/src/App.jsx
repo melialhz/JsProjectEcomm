bf4a5d6d4133771842f0e73790b7ebb383552f9a
Milia lahouazi <mlahouazi@myges.fr>
2024-07-03 11:00:59 +0200

    (+) added cart + cartItems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./Pages/Product";
 import LoginSignup from "./Pages/LoginSignup";
-
+import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
@@ -13,11 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
 
+          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
         <Footer />
