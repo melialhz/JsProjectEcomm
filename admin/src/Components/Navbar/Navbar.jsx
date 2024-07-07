4975c70b6aa6de9b7b1b0f87c6e702f3970f4fc0
Milia lahouazi <mlahouazi@myges.fr>
2024-07-08 00:43:24 +0200

    feat: add product file (+) icons

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-profile.svg'
+import React from "react";
+import "./Navbar.css";
+import navlogo from "../../assets/nav-logo.svg";
+import navProfile from "../../assets/nav-profile.png";
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <img src={navlogo} alt="" className="nav-logo" />
-            <img src={navProfile} className='nav-profile' alt="" />
+  return (
+    <div className="navbar">
+      <img src={navlogo} alt="" className="nav-logo" />
+      <img src={navProfile} className="nav-profile" alt="" />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
