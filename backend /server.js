5654041cf10b74e00c66624869e9c10a6ed5f027
Milia lahouazi <mlahouazi@myges.fr>
2024-07-08 01:18:18 +0200

    fix : server.js

--- backend /server.js
+++ backend /server.js
@@ -1,13 +1,40 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const mongoURI = process.env.MONGO_URL;
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-mongoose.connect(mongoURI)
-  .then(() => {
-    console.log('MongoDB connected...');
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    console.log('MongoDB connection error:', err);
-  });
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Routes
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+// Create Upload Endpoint for image
+app.use('/image', express.static('upload/images'))
+
+app.get("/", (req, res) => {
+    res.send("Express App is Running ...");
+});
+
+app.listen(port, (err) => {
+    if (!err) {
+        console.log("Server Running on Port " + port);
+    } else {
+        console.log("Error : " + err);
+    }
+});
